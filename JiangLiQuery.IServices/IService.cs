de9341fd787b458ba8fa117b1bf2b49f964c3bd5
myfirstnetcore <sunyxm001@hotmail.com>
2020-02-19 10:59:07 +0800

    new

---
+++ JiangLiQuery.IServices/IService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JiangLiQuery.IServices
+{
+    public interface IService<T> where T:class
+    {
+        IEnumerable<T> Query();
+
+        T Query(int id);
+
+        T Install(T newModel);
+
+        T Modify(T newModel);
+
+        T Delete(int id);
+
+    }
+}
